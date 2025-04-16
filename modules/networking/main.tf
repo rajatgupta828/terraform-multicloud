353e2af9c4c6c7d0ff89b97a320c8137523487cf
Rajat Gupta <rajatgupta828@gmail.com>
2025-04-16 18:20:20 +0530

    Networking main module

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,17 +1,31 @@
+
+#Provider to be used
 provider "aws" {
   region = var.region
 }
 
+# VPC
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+
   tags = {
     Name = var.vpc_name
   }
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-IGW"
+  }
+}
+
+#Public Subnet
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = var.public_subnet_cidrs
   map_public_ip_on_launch = true
 
   tags = {
@@ -19,11 +33,72 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+# Elastic IP for NAT
+resource "aws_eip" "nat" {
+  tags = {
+    Name = "${var.vpc_name}-NAT-EIP"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
 
   tags = {
-    Name = "${var.vpc_name}-Private Subnet"
+    Name = "${var.vpc_name}-NAT-Gateway"
   }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+
+
+# Private Subnet
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs
+  availability_zone = var.azs[0]
+
+  tags = {
+    Name = "${var.vpc_name}-Private-Subnet"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-Public-RT"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Route Table (via NAT)
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-Private-RT"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -12,3 +12,17 @@ variable "vpc_name" {
   type        = string
   description = "Name of VPC"
 }
+
+variable "public_subnet_cidrs" {
+    type        = string
+    description = "CIDR block for private subnets"
+}
+
+variable "private_subnet_cidrs" {
+  type        = string
+  description = "CIDR block for public subnets"
+}
+
+variable "azs" {
+  type = list(string)
+}
