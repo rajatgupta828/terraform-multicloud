01e8db80b09b93854bf0a52346cf86856f8ef1df
Rajat Gupta <rajatgupta828@gmail.com>
2025-04-22 12:06:33 +0530

    Add new security groups

--- environments/dev/aws/backend.tf
+++ environments/dev/aws/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    #bucket = "741018200149-terraform-resources1"
     bucket = "rajat-dev-terraform-aws"
     key    = "dev/aws/terraform.tfstate"
     region = "ap-south-1"
--- environments/dev/aws/main.tf
+++ environments/dev/aws/main.tf
@@ -24,4 +24,6 @@ module "compute" {
   private_subnet_ids = module.networking_dev.private_subnet_ids
   vpc_id            = module.networking_dev.vpc_id
   bastion_sg_id = module.networking_dev.bastion_sg_id
+  linux_2_ami = var.linux_2_ami
+  bastion_instance_type = var.bastion_instance_type
 }
